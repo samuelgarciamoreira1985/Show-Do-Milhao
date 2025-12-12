ca9ab5881be908c4ecffbddd649d23726ad54eac
Samuel Garcia Moreira <samuelgarciamoreira1985@gmail.com>
2025-12-12 17:26:53 -0300

    commit show do milhao update

--- show-do-milhao/show_do_milhao/src/assets/components/GamePlay/GamePlay.jsx
+++ show-do-milhao/show_do_milhao/src/assets/components/GamePlay/GamePlay.jsx
@@ -2,6 +2,15 @@
 import "./GamePlay.css"
 // JSON
 import data_questions from "../../../data/questions.json"
+// IMAGENS
+import cardshelp from "../../imagens/cards_help.png"
+import boardshelp from "../../imagens/boards_help.png"
+import guestshelp from "../../imagens/guests_help.png"
+import jump1help from "../../imagens/jump1_help.png"
+import jump2help from "../../imagens/jump2_help.png"
+import jump3help from "../../imagens/jump3_help.png"
+import gold from "../../imagens/gold.png"
+
 
 const GamePlay = () => {
 
@@ -14,32 +23,92 @@ const GamePlay = () => {
             {data_questions && data_questions?.map(optionQuestion => (
             <div className="questions" key={optionQuestion.id_Question}>
                 <div className="description-question">
-                  <p>{optionQuestion.question}</p>
+                  <p style={{userSelect:"none"}}>{optionQuestion.question}</p>
                 </div>
-                <div className="response">
-                    <div className="response-index"><span>A</span></div><div className="response-info">
-                      <p>{optionQuestion.response_A}</p>
-                    </div>
-                </div>
-                <div className="response">
-                    <div className="response-index"><span>B</span></div><div className="response-info">
-                      <p>{optionQuestion.response_B}</p>
-                    </div>
-                </div>
-                <div className="response">
-                    <div className="response-index"><span>C</span></div><div className="response-info">
-                      <p>{optionQuestion.response_C}</p>
-                    </div>
-                </div>
-                <div className="response">
-                    <div className="response-index"><span>D</span></div><div className="response-info">
-                      <p>{optionQuestion.response_D}</p>
-                    </div>
-                </div>
+              
+                  <div className="response">
+                      <div className="response-index"><span>A</span></div><div className="response-info">
+                        <p>{optionQuestion.response_A}</p>
+                      </div>
+                  </div>
+                  <div className="response">
+                      <div className="response-index"><span>B</span></div><div className="response-info">
+                        <p>{optionQuestion.response_B}</p>
+                      </div>
+                  </div>
+                  <div className="response">
+                      <div className="response-index"><span>C</span></div><div className="response-info">
+                        <p>{optionQuestion.response_C}</p>
+                      </div>
+                  </div>
+                  <div className="response">
+                      <div className="response-index"><span>D</span></div><div className="response-info">
+                        <p>{optionQuestion.response_D}</p>
+                      </div>
+                  </div>
+                
             </div>
             ))}
 
             <div className="help">
+
+                <div className="button-stop-game">
+                    <button type="button">PARAR</button>
+                </div>
+
+                <div className="group-items-help">
+                  <h3>AJUDA</h3>
+                    <div className="items-1">
+                        <div className="items-1-details">
+                         <img src={cardshelp} alt="imagem - cartas" />
+                          <p>Cartas</p>
+                        </div>
+                        <div className="items-1-details">
+                          <img src={boardshelp} alt="imagem - placas" />
+                          <p>Placas</p>
+                        </div>
+                        <div className="items-1-details">
+                          <img src={guestshelp} alt="imagem - convidados" />
+                          <p>Convidados</p>
+                        </div>
+                    </div>
+
+                    <div className="items-2">
+                        <div className="items-2-details">
+                         <img src={jump1help} alt="imagem - pulo 1" />
+                          <p>Pulo 1</p>
+                        </div>
+                        <div className="items-2-details">
+                          <img src={jump2help} alt="imagem - pulo 2" />
+                          <p>Pulo 2</p>
+                        </div>
+                        <div className="items-2-details">
+                          <img src={jump3help} alt="imagem - pulo 3" />
+                          <p>Pulo 3</p>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div className="score-money">
+
+                <div className="score-money-details">
+                    <p className="value-money">500</p>
+                    <img src={gold} alt="imagem - Barra de ouro - Errar" />
+                    <p>Errar</p>
+                </div>
+                <div className="score-money-details">
+                    <p className="value-money">1 MIL</p>
+                    <img src={gold} alt="imagem - Barra de ouro - Errar" />
+                    <p>Parar</p>
+                </div>
+                <div className="score-money-details">
+                    <p className="value-money">2 MIL</p>
+                    <img src={gold} alt="imagem - Barra de ouro - Errar" />
+                    <p>Acertar</p>
+                </div>
 
             </div>
 
