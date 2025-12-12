2b337e74b1c27a6b76d9a36c6a89f76342a27f14
Samuel Garcia Moreira <samuelgarciamoreira1985@gmail.com>
2025-12-12 13:26:24 -0300

    commit show do milhao game play

--- show-do-milhao/show_do_milhao/src/assets/components/GamePlay/GamePlay.jsx
+++ show-do-milhao/show_do_milhao/src/assets/components/GamePlay/GamePlay.jsx
@@ -6,7 +6,41 @@ const GamePlay = () => {
   return (
 
     <div className="container-gameplay">
-      <h2>GAME PLAY</h2>
+      
+        <div className="sub-container-gameplay">
+
+            <div className="questions">
+                <div className="description-question">
+                  <p>PERGUNTA</p>
+                </div>
+                <div className="response">
+                    <div className="response-index"><span>A</span></div><div className="response-info">
+                      <p>RESPOSTA 1</p>
+                    </div>
+                </div>
+                <div className="response">
+                    <div className="response-index"><span>B</span></div><div className="response-info">
+                      <p>RESPOSTA 2</p>
+                    </div>
+                </div>
+                <div className="response">
+                    <div className="response-index"><span>C</span></div><div className="response-info">
+                      <p>RESPOSTA 3</p>
+                    </div>
+                </div>
+                <div className="response">
+                    <div className="response-index"><span>D</span></div><div className="response-info">
+                      <p>RESPOSTA 4</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="help">
+
+            </div>
+
+        </div>
+
     </div>
 
   )
